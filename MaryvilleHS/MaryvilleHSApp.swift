4bbea1f087a102b1b6088b156a8bb77987227215
Jevon Mao <woodburyjevonmao@gmail.com>
2022-10-16 12:33:18 -0700

    Initial Commit

---
+++ MaryvilleHS/MaryvilleHSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaryvilleHSApp.swift
+//  MaryvilleHS
+//
+//  Created by Jevon Mao on 10/16/22.
+//
+
+import SwiftUI
+
+@main
+struct MaryvilleHSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
