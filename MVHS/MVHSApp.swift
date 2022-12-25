87a4be66f65b50c6acd19ca04f30f9e4792a13c7
Jevon Mao <woodburyjevonmao@gmail.com>
2022-12-25 12:10:03 -0800

    Initial Commit

---
+++ MVHS/MVHSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVHSApp.swift
+//  MVHS
+//
+//  Created by Jevon Mao on 12/25/22.
+//
+
+import SwiftUI
+
+@main
+struct MVHSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
